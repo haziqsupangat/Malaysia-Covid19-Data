079c4dc7ce40a4a54791c42d42adc5ee9c929fbc
serotonin <70641992+haziqsupangat@users.noreply.github.com>
2021-01-24 13:00:21 +0800

    Create Graph of total cases.R

---
+++ Graph/Graph of total cases.R
@@ -0,0 +1,32 @@
+library(dplyr)
+library(data.table)
+library(tidyverse)
+library(ggplot2)
+library(RCurl)
+library(lubridate)
+
+
+#Data Source
+jhu_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+            
+#Filter country and Modify table to remove unnecessary data
+malaysia_case <- read.csv(url(jhu_url), header = TRUE, row.names = ) %>%
+  filter(Country.Region == "Malaysia") %>%
+  pivot_longer(-c(Province.State, Country.Region, Lat, Long), names_to = "Date", values_to = "Cumulative Cases") %>%
+  rename(Country = "Country.Region") %>%
+  ungroup() %>% 
+  select(-c(Province.State, Lat, Long))
+ 
+
+#Change date format
+Dates <- gsub("X", "", malaysia_case$Date)
+Date_num <- as.Date(Date, format = "%m.%d.%y")
+
+
+#Create data frame
+Table <- data.frame(Date = Date_num, Cases = malaysia_case$`Cumulative Cases`)
+
+
+#Plotting output
+plot <- ggplot(Table, mapping = aes(x = Date, y = Cases))  + geom_line()
+print(plot)
